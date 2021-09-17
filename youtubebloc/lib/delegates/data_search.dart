18e8a29f5c679ebd301b50d474a86cdf7847612b
Rafael <rafael.vieira.abranttes@gmail.com>
2021-09-16 23:34:03 -0300

    ¨finalizando¨

--- youtubebloc/lib/delegates/data_search.dart
+++ youtubebloc/lib/delegates/data_search.dart
@@ -4,14 +4,15 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class DataSearch extends SearchDelegate<String> {
+
   @override
   List<Widget> buildActions(BuildContext context) {
     return [
       IconButton(
-        onPressed: () {
+        icon: Icon(Icons.clear),
+        onPressed: (){
           query = "";
         },
-        icon: Icon(Icons.clear),
       )
     ];
   }
@@ -19,66 +20,67 @@ class DataSearch extends SearchDelegate<String> {
   @override
   Widget buildLeading(BuildContext context) {
     return IconButton(
-      onPressed: () {
+      icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_arrow,
+          progress: transitionAnimation
+      ),
+      onPressed: (){
         close(context, null);
       },
-      icon: AnimatedIcon(
-        icon: AnimatedIcons.menu_arrow,
-        progress: transitionAnimation,
-      ),
     );
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    Future.delayed(Duration.zero).then(
-      (_) => close(context, query),
-    );
+    Future.delayed(Duration.zero).then((_)=>close(context, query));
 
     return Container();
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    if (query.isEmpty)
+    if(query.isEmpty)
       return Container();
     else
       return FutureBuilder<List>(
-        future: suggestion(query),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
+        future: suggestions(query),
+        builder: (context, snapshot){
+          if(!snapshot.hasData){
             return Center(
               child: CircularProgressIndicator(),
             );
           } else {
             return ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(
-                      snapshot.data[index],
-                    ),
-                    leading: Icon(Icons.play_arrow),
-                    onTap: () {
-                      close(context, snapshot.data[index]);
-                    },
-                  );
-                });
+              itemBuilder: (context, index){
+                return ListTile(
+                  title: Text(snapshot.data[index]),
+                  leading: Icon(Icons.play_arrow),
+                  onTap: (){
+                    close(context, snapshot.data[index]);
+                  },
+                );
+              },
+              itemCount: snapshot.data.length,
+            );
           }
         },
       );
   }
 
-  Future<List> suggestion(String search) async {
-    http.Response response = await http.get(
-        "http://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&cp=1&q=$search&format=5&alt=json");
+  Future<List> suggestions(String search) async {
 
-    if (response.statusCode == 200) {
-      return json.decode(response.body)[1].map((v) {
+    http.Response response = await http.get(
+        "http://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&cp=1&q=$search&format=5&alt=json"
+    );
+
+    if(response.statusCode == 200){
+      return json.decode(response.body)[1].map((v){
         return v[0];
       }).toList();
     } else {
       throw Exception("Failed to load suggestions");
     }
+
   }
+
 }
